54818d56817102058d493171e482b71ec2927552
Evan Flynn <erflynn21@gmail.com>
2020-12-21 15:08:32 +0800

    adds feature toggle

--- services/web/src/client.js
+++ services/web/src/client.js
@@ -1,4 +1,6 @@
 import * as sapper from '@sapper/app';
+import { initFeatureToggles } from './feature-toggles/index';
+initFeatureToggles();
 
 sapper.start({
 	target: document.querySelector('#sapper')
