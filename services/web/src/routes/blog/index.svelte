33bde7a89a2e33f676d97511e3d756768737d4e5
Evan Flynn <erflynn21@gmail.com>
2020-12-22 14:50:10 +0800

    updates blog route

--- services/web/src/routes/blog/index.svelte
+++ services/web/src/routes/blog/index.svelte
@@ -36,11 +36,13 @@
 <h1>Recent posts</h1>
 
 <ul data-cy="blog-posts-list">
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
-	{/each}
+	{#if posts}
+		{#each posts as post}
+			<!-- we're using the non-standard `rel=prefetch` attribute to
+					tell Sapper to load the data for the page as soon as
+					the user hovers over the link or taps it, instead of
+					waiting for the 'click' event -->
+			<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+		{/each}
+	{/if}
 </ul>
